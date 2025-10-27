55e95762218c58c27e7acf5cc77687cb1d02e6a5
kyawzayhan26 <kyawzh.steven@gmail.com>
2025-10-28 07:35:14 +0800

    changed scchema sql script

--- backend/sql/schema_full_v6_final.sql
+++ backend/sql/schema_full_v6_final.sql
@@ -46,6 +46,7 @@ CREATE TABLE dbo.Users (
     email          NVARCHAR(150)  NOT NULL UNIQUE,
     password_hash  NVARCHAR(255)  NOT NULL,
     full_name      NVARCHAR(150)  NOT NULL,
+    name           AS full_name,
     is_active      BIT            NOT NULL DEFAULT 1,
     created_at     DATETIME2      NOT NULL DEFAULT SYSUTCDATETIME(),
     updated_at     DATETIME2      NULL
@@ -185,7 +186,7 @@ CREATE TABLE dbo.ProspectJobMatches (
     is_current   BIT           NOT NULL DEFAULT 1,
     created_at   DATETIME2     NOT NULL DEFAULT SYSUTCDATETIME(),
     updated_at   DATETIME2     NOT NULL DEFAULT SYSUTCDATETIME(),
-    insDeleted   BIT           NOT NULL DEFAULT 0,
+    isDeleted   BIT           NOT NULL DEFAULT 0,
     CONSTRAINT FK_ProspectJobMatches_Prospects FOREIGN KEY (prospect_id) REFERENCES dbo.Prospects(id) ON DELETE CASCADE,
     CONSTRAINT FK_ProspectJobMatches_Jobs FOREIGN KEY (job_id) REFERENCES dbo.Jobs(id),
     CONSTRAINT FK_ProspectJobMatches_Users FOREIGN KEY (matched_by) REFERENCES dbo.Users(id)
